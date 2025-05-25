41f81b6153cd4cab9fae4adac610dded9e6a3e1a
Iman Lanjaby Sharahy <imanaminir@gmail.com>
2025-05-25 19:57:53 +0330

    cpu amd MB datas added

---
+++ test data/MB.py
@@ -0,0 +1,107 @@
+motherBoards =[
+
+    {"name": "ASUS ROG MAXIMUS Z790 HERO","brand": "ASUS","price" :599},
+    {"name": "ASUS TUF Gaming Z790-PLUS WIFI D4","brand": "ASUS","price" :269},
+    {"name": "ASUS PRIME Z790-P WIFI","brand": "ASUS","price" :209},
+    {"name": "MSI MEG Z790 GODLIKE","brand": "MSI","price" :699},
+    {"name": "MSI MPG Z790 CARBON WIFI","brand": "MSI","price" :409},
+    {"name": "MSI PRO Z790-A WIFI","brand": "MSI","price" :229},
+    {"name": "Gigabyte Z790 AORUS MASTER","brand": "Gigabyte","price" :449},
+    {"name": "Gigabyte Z790 AORUS ELITE AX","brand": "Gigabyte","price" :269},
+    {"name": "Gigabyte Z790 UD AC","brand": "Gigabyte","price" :189},
+    {"name": "ASRock Z790 Taichi","brand": "ASRock","price" :369},
+    {"name": "ASRock Z790 PG Lightning","brand": "ASRock","price" :209},
+    {"name": "ASRock Z790 Steel Legend WIFI","brand": "ASRock","price" :249},
+
+
+    {"name": "ASUS TUF Gaming B760-PLUS WIFI D4","brand": "ASUS","price" :195},
+    {"name": "ASUS PRIME B760M-A WIFI D4","brand": "ASUS","price" :165},
+    {"name": "ASUS ROG STRIX B760-F GAMING WIFI","brand": "ASUS","price" :225},
+    {"name": "MSI PRO B760M-A WIFI DDR4","brand": "MSI","price" :145},
+    {"name": "MSI MAG B760 TOMAHAWK WIFI","brand": "MSI","price" :215},
+    {"name": "MSI MPG B760I EDGE WIFI DDR4","brand": "MSI","price" :189},
+    {"name": "Gigabyte B760M DS3H AX DDR4","brand": "Gigabyte","price" :139},
+    {"name": "Gigabyte B760 AORUS ELITE AX","brand": "Gigabyte","price" :199},
+    {"name": "Gigabyte B760M Gaming X AX DDR4","brand": "Gigabyte","price" :159},
+    {"name": "ASRock B760M PRO RS/D4","brand": "ASRock","price" :139},
+    {"name": "ASRock B760M Steel Legend WIFI","brand": "ASRock","price" :175},
+    {"name": "ASRock B760M PG Riptide","brand": "ASRock","price" :159},
+
+
+    {"name": "ASUS ROG MAXIMUS Z690 HERO","brand": "ASUS","price" :549},
+    {"name": "ASUS TUF Gaming Z690-PLUS WIFI D4","brand": "ASUS","price" :255},
+    {"name": "MSI MPG Z690 CARBON WIFI","brand": "MSI","price" :379},
+    {"name": "Gigabyte Z690 AORUS ELITE AX DDR4","brand": "Gigabyte","price" :239},
+    {"name": "ASRock Z690 Extreme WIFI 6E","brand": "ASRock","price" :269},
+
+
+    {"name": "ASUS PRIME B660M-A WIFI D4","brand": "ASUS","price" :145},
+    {"name": "ASUS TUF Gaming B660M-PLUS WIFI D4","brand": "ASUS","price" :165},
+    {"name": "MSI PRO B660M-A WIFI DDR4","brand": "MSI","price" :135},
+    {"name": "Gigabyte B660M DS3H AX DDR4","brand": "Gigabyte","price" :129},
+    {"name": "ASRock B660M Pro RS","brand": "ASRock","price" :119},
+
+
+    {"name": "ASUS ROG MAXIMUS XIII HERO Z590","brand": "ASUS","price" :439},
+    {"name": "MSI MPG Z590 GAMING EDGE WIFI","brand": "MSI","price" :289},
+    {"name": "Gigabyte Z590 AORUS ELITE AX","brand": "Gigabyte","price" :269},
+    {"name": "ASRock Z590 Extreme WIFI 6E","brand": "ASRock","price" :239},
+
+
+    {"name": "ASUS TUF Gaming B560M-PLUS WIFI","brand": "ASUS","price" :125},
+    {"name": "MSI B560M PRO-VDH WIFI","brand": "MSI","price" :115},
+    {"name": "Gigabyte B560M DS3H AC","brand": "Gigabyte","price" :109},
+    {"name": "ASRock B560M Steel Legend","brand": "ASRock","price" :119},
+
+
+    {"name": "ASUS ROG CROSSHAIR X670E HERO","brand": "ASUS","price" :519},
+    {"name": "ASUS PRIME X670-P WIFI","brand": "ASUS","price" :289},
+    {"name": "ASUS TUF Gaming X670E-PLUS WIFI","brand": "ASUS","price" :319},
+    {"name": "MSI MEG X670E ACE","brand": "MSI","price" :489},
+    {"name": "MSI PRO X670-P WIFI","brand": "MSI","price" :269},
+    {"name": "MSI MPG X670E CARBON WIFI","brand": "MSI","price" :369},
+    {"name": "Gigabyte X670 AORUS ELITE AX","brand": "Gigabyte","price" :309},
+    {"name": "Gigabyte X670E AORUS MASTER","brand": "Gigabyte","price" :449},
+    {"name": "ASRock X670E Steel Legend","brand": "ASRock","price" :319},
+    {"name": "ASRock X670E Pro RS","brand": "ASRock","price" :279},
+
+
+
+    {"name": "ASUS TUF Gaming B650-PLUS WIFI","brand": "ASUS","price" :225},
+    {"name": "ASUS PRIME B650M-A WIFI","brand": "ASUS","price" :189},
+    {"name": "ASUS ROG STRIX B650-A GAMING WIFI","brand": "ASUS","price" :259},
+    {"name": "MSI PRO B650M-A WIFI","brand": "MSI","price" :179},
+    {"name": "MSI MPG B650 EDGE WIFI","brand": "MSI","price" :239},
+    {"name": "MSI MAG B650 TOMAHAWK WIFI","brand": "MSI","price" :219},
+    {"name": "Gigabyte B650 AORUS ELITE AX","brand": "Gigabyte","price" :229},
+    {"name": "Gigabyte B650M DS3H AX","brand": "Gigabyte","price" :179},
+    {"name": "ASRock B650 PG Lightning","brand": "ASRock","price" :199},
+    {"name": "ASRock B650M Pro RS","brand": "ASRock","price" :169},
+
+
+    {"name": "ASUS ROG CROSSHAIR VIII HERO (WI-FI)","brand": "ASUS","price" :359},
+    {"name": "ASUS PRIME X570-PRO","brand": "ASUS","price" :259},
+    {"name": "ASUS TUF Gaming X570-PLUS WIFI","brand": "ASUS","price" :219},
+    {"name": "MSI MEG X570 ACE","brand": "MSI","price" :379},
+    {"name": "MSI MPG X570 GAMING EDGE WIFI","brand": "MSI","price" :239},
+    {"name": "MSI MAG X570 TOMAHAWK WIFI","brand": "MSI","price" :249},
+    {"name": "Gigabyte X570 AORUS MASTER","brand": "Gigabyte","price" :349},
+    {"name": "Gigabyte X570 AORUS ELITE WIFI","brand": "Gigabyte","price" :219},
+    {"name": "ASRock X570 Steel Legend","brand": "ASRock","price" :219},
+    {"name": "ASRock X570 Phantom Gaming 4","brand": "ASRock","price" :189},
+
+
+    {"name": "ASUS ROG STRIX B550-F GAMING WIFI II","brand": "ASUS","price" :219},
+    {"name": "ASUS PRIME B550M-A WIFI II","brand": "ASUS","price" :179},
+    {"name": "ASUS TUF GAMING B550-PLUS","brand": "ASUS","price" :189},
+    {"name": "MSI MAG B550 TOMAHAWK","brand": "MSI","price" :199},
+    {"name": "MSI B550M PRO-VDH WIFI","brand": "MSI","price" :149},
+    {"name": "MSI MPG B550 GAMING PLUS","brand": "MSI","price" :179},
+    {"name": "Gigabyte B550 AORUS PRO AC","brand": "Gigabyte","price" :209},
+    {"name": "Gigabyte B550M DS3H AC","brand": "Gigabyte","price" :139},
+    {"name": "ASRock B550 Steel Legend","brand": "ASRock","price" :189},
+    {"name": "ASRock B550 Phantom Gaming 4","brand": "ASRock","price" :149},
+
+]
+
+
---
+++ test data/cpu.py
@@ -0,0 +1,67 @@
+from yourapp.models import Product, Part
+
+cpu_part, _ = Part.objects.get_or_create(name='CPU')
+
+cpus = [
+    # Intel Core i9 Series
+    {"name": "Intel Core i9-14900K", "brand": "Intel", "price": 589},
+    {"name": "Intel Core i9-13900K", "brand": "Intel", "price": 569},
+    {"name": "Intel Core i9-12900K", "brand": "Intel", "price": 499},
+    {"name": "Intel Core i9-11900K", "brand": "Intel", "price": 439},
+    {"name": "Intel Core i9-10900K", "brand": "Intel", "price": 429},
+
+    # Intel Core i7 Series
+    {"name": "Intel Core i7-14700K", "brand": "Intel", "price": 409},
+    {"name": "Intel Core i7-13700K", "brand": "Intel", "price": 389},
+    {"name": "Intel Core i7-12700K", "brand": "Intel", "price": 379},
+    {"name": "Intel Core i7-11700K", "brand": "Intel", "price": 359},
+    {"name": "Intel Core i7-10700K", "brand": "Intel", "price": 349},
+
+    # Intel Core i5 Series
+    {"name": "Intel Core i5-14600K", "brand": "Intel", "price": 319},
+    {"name": "Intel Core i5-13600K", "brand": "Intel", "price": 299},
+    {"name": "Intel Core i5-12600K", "brand": "Intel", "price": 279},
+    {"name": "Intel Core i5-11600K", "brand": "Intel", "price": 249},
+    {"name": "Intel Core i5-10600K", "brand": "Intel", "price": 229},
+    {"name": "Intel Core i5-12400F", "brand": "Intel", "price": 179},
+    {"name": "Intel Core i5-11400", "brand": "Intel", "price": 169},
+    {"name": "Intel Core i5-10400", "brand": "Intel", "price": 159},
+
+    # Intel Core i3 Series
+    {"name": "Intel Core i3-13100", "brand": "Intel", "price": 149},
+    {"name": "Intel Core i3-12100", "brand": "Intel", "price": 129},
+    {"name": "Intel Core i3-10100", "brand": "Intel", "price": 119},
+
+    # AMD Ryzen 9
+    {"name": "AMD Ryzen 9 7950X", "brand": "AMD", "price": 699},
+    {"name": "AMD Ryzen 9 7900X", "brand": "AMD", "price": 549},
+    {"name": "AMD Ryzen 9 5900X", "brand": "AMD", "price": 549},
+    {"name": "AMD Ryzen 9 3950X", "brand": "AMD", "price": 749},
+
+    # AMD Ryzen 7
+    {"name": "AMD Ryzen 7 7800X3D", "brand": "AMD", "price": 449},
+    {"name": "AMD Ryzen 7 7700X", "brand": "AMD", "price": 399},
+    {"name": "AMD Ryzen 7 5800X", "brand": "AMD", "price": 399},
+    {"name": "AMD Ryzen 7 5700X", "brand": "AMD", "price": 329},
+    {"name": "AMD Ryzen 7 3800X", "brand": "AMD", "price": 359},
+
+    # AMD Ryzen 5
+    {"name": "AMD Ryzen 5 7600X", "brand": "AMD", "price": 299},
+    {"name": "AMD Ryzen 5 5600X", "brand": "AMD", "price": 299},
+    {"name": "AMD Ryzen 5 5600", "brand": "AMD", "price": 199},
+    {"name": "AMD Ryzen 5 3600", "brand": "AMD", "price": 189},
+    {"name": "AMD Ryzen 5 2600", "brand": "AMD", "price": 149},
+
+    # AMD Ryzen 3
+    {"name": "AMD Ryzen 3 4100", "brand": "AMD", "price": 99},
+    {"name": "AMD Ryzen 3 3200G", "brand": "AMD", "price": 79},
+
+]
+
+for cpu in cpus:
+    Product.objects.get_or_create(
+        name=cpu["name"],
+        part=cpu_part,
+        brand=cpu["brand"],
+        price=cpu["price"]
+    )
