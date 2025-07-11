516c33590735452d2118b83e88d876d1ea13837a
Iman Lanjaby Sharahy <imanaminir@gmail.com>
2025-07-11 05:50:37 +0330

    specs for cpu cooler added
    power connecror spec updated
    
    MB connectors updated

--- test data/Cooler.py
+++ test data/Cooler.py
@@ -40,3 +40,94 @@ for Cooler in Coolers:
         brand=Cooler["brand"],
         price=Cooler["price"]
     )
+
+
+coolers_specs = [
+    # AIO Liquid Coolers
+    {
+        "name": "Corsair iCUE H100i RGB Elite 240mm",
+        "type": "AIO Liquid",
+        "tdp capacity": 250,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "NZXT Kraken X53 RGB 240mm",
+        "type": "AIO Liquid",
+        "tdp capacity": 240,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "Cooler Master MasterLiquid ML240L V2",
+        "type": "AIO Liquid",
+        "tdp capacity": 200,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "DeepCool LS520 240mm",
+        "type": "AIO Liquid",
+        "tdp capacity": 260,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "ARCTIC Liquid Freezer II 240mm",
+        "type": "AIO Liquid",
+        "tdp capacity": 270,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "Thermaltake TH360 ARGB Sync 360mm",
+        "type": "AIO Liquid",
+        "tdp capacity": 280,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "Lian Li Galahad II Trinity 240mm",
+        "type": "AIO Liquid",
+        "tdp capacity": 275,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+
+    # Air Coolers
+    {
+        "name": "Cooler Master Hyper 212 Black Edition",
+        "type": "Air",
+        "tdp capacity": 150,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "DeepCool GAMMAXX 400 V2",
+        "type": "Air",
+        "tdp capacity": 130,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "be quiet! Pure Rock 2 Black",
+        "type": "Air",
+        "tdp capacity": 150,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "Noctua NH-U12S Redux",
+        "type": "Air",
+        "tdp capacity": 180,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "Thermalright Assassin X 120 SE",
+        "type": "Air",
+        "tdp capacity": 160,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "ARCTIC Freezer 34 eSports DUO",
+        "type": "Air",
+        "tdp capacity": 170,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    },
+    {
+        "name": "Vetroo V5 Black",
+        "type": "Air",
+        "tdp capacity": 140,
+        "compatible sockets": ["Intel LGA1700", "Intel LGA1200", "Intel LGA115x", "AMD AM5", "AMD AM4"]
+    }
+]
